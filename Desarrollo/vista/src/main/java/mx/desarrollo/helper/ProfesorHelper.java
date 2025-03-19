1fb20643b62d5ccfeece3a50830f96952b1943e1
Alan J. Ruiz <144744709+ServBot213@users.noreply.github.com>
2025-03-18 19:21:26 -0700

    Lo mejor que pude hacer ahorita
    Pues, aun faltan mostrar consultas y todo, pero almenos la interfaz funciona

---
+++ Desarrollo/vista/src/main/java/mx/desarrollo/helper/ProfesorHelper.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.desarrollo.helper;
+
+import java.io.Serializable;
+import java.util.List;
+import mx.desarrollo.entidad.Profesor;
+import mx.desarrollo.integracion.ServiceFacadeLocator;
+
+/**
+ *
+ * @author Kevin
+ */
+public class ProfesorHelper implements Serializable {
+    
+    /**
+     * Metodo para obtener un profesor por su ID
+     * @param id
+     * @return Profesor encontrado o null si no existe
+     */
+    public Profesor obtenerProfesorPorId(int id) {
+        return ServiceFacadeLocator.getInstanceFacadeProfesor().buscarPorId(id);
+    }
+    
+    /**
+     * Metodo para guardar un profesor
+     * @param profesor
+     */
+    public void guardarProfesor(Profesor profesor) {
+        ServiceFacadeLocator.getInstanceFacadeProfesor().guardar(profesor);
+    }
+    
+    /**
+     * Metodo para eliminar un profesor por su ID
+     * @param id
+     */
+    public void eliminarProfesor(int id) {
+        ServiceFacadeLocator.getInstanceFacadeProfesor().eliminar(id);
+    }
+    
+    public List<Profesor> obtenerTodosLosProfesores() {
+        return ServiceFacadeLocator.getInstanceFacadeProfesor().buscarTodos();
+    }
+}
